8534eebc962ab43ae363bf31d5c98ac5068b553f
Kevin Grandemange <grandemange.kevin@gmail.com>
2020-04-18 11:07:27 +0200

    Add First assets and character controls + test scene

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        transform.Translate(Time.deltaTime * speed * direction);
+    }
+}
