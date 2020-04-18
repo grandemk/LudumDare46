668f85052ed1c47a900c4013bbdb2960e65eddbc
Kevin Grandemange <grandemange.kevin@gmail.com>
2020-04-18 18:48:55 +0200

    Fix webgl export

--- Assets/Editor/WebGLBuilder.cs
+++ Assets/Editor/WebGLBuilder.cs
@@ -3,7 +3,7 @@ using UnityEditor;
 class WebGLBuilder {
     static void build()
     {
-        string[] scenes = {"Assets/Scenes/SampleScene.unity"};
+        string[] scenes = {"Assets/Scenes/Game.unity"};
         string deployPath = "builds/WebGL/";
 
         BuildPipeline.BuildPlayer(scenes, deployPath, BuildTarget.WebGL, BuildOptions.None);
